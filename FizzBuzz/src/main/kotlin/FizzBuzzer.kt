a5b7631c686ba774ea5532247d7148eaead1f50f
Jose Wenzel <devwenzel@gmail.com>
2022-11-29 00:44:08 +0100

    feat(fizz-buzz): implement full program and remove duplication

--- FizzBuzz/src/main/kotlin/FizzBuzzer.kt
+++ FizzBuzz/src/main/kotlin/FizzBuzzer.kt
@@ -1,6 +1,13 @@
 class FizzBuzzer(private val printer: Printer) {
     fun calculate(number: Int) {
-        if (number % 3 == 0) printer.printLine("Fizz")
-        printer.printLine(number.toString())
+        when {
+            number.isDivisibleBy(15) -> printer.printLine("FizzBuzz")
+            number.isDivisibleBy(5) -> printer.printLine("Buzz")
+            number.isDivisibleBy(3) -> printer.printLine("Fizz")
+            else -> printer.printLine(number.toString())
+        }
     }
 }
+
+fun Int.isDivisibleBy(value: Int): Boolean = this % value == 0
+
