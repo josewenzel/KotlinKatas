f755e20c864e60314a4fdf9edc4b06df3bc2d72b
Jose Wenzel <devwenzel@gmail.com>
2022-11-28 23:42:07 +0100

    feat(fizz-buzz): return fizz for 4

--- FizzBuzz/src/main/kotlin/FizzBuzzer.kt
+++ FizzBuzz/src/main/kotlin/FizzBuzzer.kt
@@ -1,5 +1,6 @@
 class FizzBuzzer(private val printer: Printer) {
     fun calculate(number: Int) {
+        if (number % 3 == 0) printer.printLine("Fizz")
         printer.printLine(number.toString())
     }
 }
