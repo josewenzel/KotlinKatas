dbdcb9bdfb352af614d7cb6cbdd70a43a9c864c3
Jose Wenzel <devwenzel@gmail.com>
2022-12-03 23:04:04 +0100

    chore(roman-numerals): initialise repository

---
+++ RomanNumerals/build.gradle.kts
@@ -0,0 +1,26 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.21"
+}
+
+group = "org.josewenzel"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.strikt:strikt-core:0.34.1")
+    implementation("io.mockk:mockk:1.13.2")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
