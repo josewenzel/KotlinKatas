de33e6c176daf4616d42354b4faf96cf83b2f9af
Jose Wenzel <devwenzel@gmail.com>
2022-12-04 00:40:10 +0100

    feat(roman-numerals): solve all symbols

--- RomanNumerals/src/test/kotlin/ToRomanTest.kt
+++ RomanNumerals/src/test/kotlin/ToRomanTest.kt
@@ -1,17 +1,48 @@
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.DynamicTest.dynamicTest
+import org.junit.jupiter.api.TestFactory
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
 
 class ToRomanTest {
-    @Test
-    fun `that 1 returns I`() {
-        val result = 1.toRoman()
-        expectThat(result).isEqualTo("I")
-    }
+    private val cases = listOf(
+        1 to "I",
+        2 to "II",
+        3 to "III",
+        4 to "IV",
+        5 to "V",
+        6 to "VI",
+        7 to "VII",
+        8 to "VIII",
+        9 to "IX",
+        10 to "X",
+        11 to "XI",
+        12 to "XII",
+        13 to "XIII",
+        14 to "XIV",
+        15 to "XV",
+        40 to "XL",
+        50 to "L",
+        90 to "XC",
+        100 to "C",
+        400 to "CD",
+        500 to "D",
+        900 to "CM",
+        1000 to "M"
+    )
 
-    @Test
-    fun `that 2 returns II`() {
-        val result = 2.toRoman()
-        expectThat(result).isEqualTo("II")
-    }
+    @TestFactory
+    fun `that toRoman returns`() =
+        cases.map { (arabic, romanConversion) ->
+            dynamicTest("$romanConversion for $arabic") {
+                expectThat(arabic.toRoman()).isEqualTo(romanConversion)
+            }
+        }
+
+    @TestFactory
+    fun `that toRomanRecursive returns`() =
+        cases.map { (arabic, romanConversion) ->
+            dynamicTest("$romanConversion for $arabic") {
+                expectThat(arabic.toRoman2()).isEqualTo(romanConversion)
+            }
+        }
 }
