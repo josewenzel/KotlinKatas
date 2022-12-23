4c6fde71e3495ac21126377a9d688f8414923493
Jose Wenzel <devwenzel@gmail.com>
2022-12-23 08:59:18 -0500

    refactor(all): apply ktlint

--- LeapYear/src/main/kotlin/Year.kt
+++ LeapYear/src/main/kotlin/Year.kt
@@ -3,7 +3,7 @@ typealias Year = Int
 fun Int.isDivisibleBy(divisor: Int) = this % divisor == 0
 
 fun Year.isLeap(): Boolean {
-    return this.isDivisibleBy(4)
-            && !this.isDivisibleBy(100)
-            || this.isDivisibleBy(400)
+    return this.isDivisibleBy(4) &&
+        !this.isDivisibleBy(100) ||
+        this.isDivisibleBy(400)
 }
